5ecbefd8b55322b05dc79ea65e8cbcb21e9b039f
Artem <artemvaschenko83@gmail.com>
2025-09-20 14:22:38 +0300

    Add posts page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-let moment = require("moment");
+const moment = require("moment");
+const path = require("path");
+const fs = require("fs");
 
 function getCurrentTime(){
     let currentTime = moment().format("LTS");
@@ -7,16 +9,23 @@ function getCurrentTime(){
 }
 
 const express = require("express");
+const { json } = require("stream/consumers");
 
 const HOST = "127.0.0.1";
 const PORT = "8000";
 
 const app = express();
 
+const jsonPath = path.join(__dirname, "products.json");
 app.get("/timestamp", (req, res) => {
     let time = getCurrentTime();
     res.status(200).json({"currentTime": time});
 })
+app.get("/posts", (req, res) => {
+    const products = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
+    res.status(200).json(products);
+})
 app.listen(PORT, HOST, () => {
-    console.log(`http://${HOST}:${PORT}/timestamp`);
+    console.log(`Час: \nhttp://${HOST}:${PORT}/timestamp\n`);
+    console.log(`Пости: \nhttp://${HOST}:${PORT}/posts`);
 })
