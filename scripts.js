6e142f840bfa68197f477988fe0eeb397c2bb914
Artem <artemvaschenko83@gmail.com>
2025-10-02 22:46:26 +0300

    Now you can add a lot of objects to posts at one request

--- scripts.js
+++ scripts.js
@@ -58,24 +58,28 @@ app.get("/posts", (req, res) => {
     res.status(200).json(slicedPosts);
 })
 app.post("/posts", async (req, res) => {
-    try{
-        const data = req.body;
-        if (!data.title || !data.description || !data.image){
+    let data = req.body;
+    if (!Array.isArray(data)){
+        data = [req.body];
+    }
+    let newPosts = [];
+    for (let addedPost of data){
+        if (!addedPost.title || !addedPost.description || !addedPost.image){
             res.status(422).json("Please, enter data correctly!");
             return;
         }
         let postId = 0;
-        if (posts.length > 0){
+        if (posts.length > 0){  
             postId = posts[posts.length - 1].id + 1;
         }
-        const newPost = { id: postId, ...data};
-        posts.push(newPost);    
+        const newPost = { id: postId, ...addedPost };
+        newPosts.push(newPost);
+        posts.push(newPost);
         await fsPromises.writeFile(postsJson, [JSON.stringify(posts)]);
-        res.status(200).json(newPost);
-    }catch (error){
-        console.log("error")
-        res.status(500).json(error);
     }
+    
+    // posts.push(newPost);    
+    res.status(200).json(newPosts);
 })
 app.get("/posts/:id", (req, res) => {
     const postId = req.params.id;
